88d0f720c1ad24a2fc3c33341dd233bd8c670aed
aalan <avalosalan789@gmail.com>
2024-02-18 09:14:10 -0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+//Radix
+import "@radix-ui/themes/styles.css";
+
+//Theme
+import { Theme } from "@radix-ui/themes";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
